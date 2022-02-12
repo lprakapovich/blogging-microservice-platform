1139eb77dae94611564f0413cb604f930c50b804
Liza <audlisab@gmail.com>
2022-02-12 16:40:45 +0100

    remove remaining code with user firstname, lastname

--- authorization-service/src/main/java/com/lprakapovich/authorizationservice/api/dto/RegisterDto.java
+++ authorization-service/src/main/java/com/lprakapovich/authorizationservice/api/dto/RegisterDto.java
@@ -3,20 +3,13 @@ package com.lprakapovich.authorizationservice.api.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Data
 public class RegisterDto {
 
-    @NotEmpty(message = "Username cannot be empty")
+    @NotBlank(message = "Username cannot be empty")
     private String username;
 
-    @NotEmpty(message = "Password cannot be empty")
+    @NotBlank(message = "Password cannot be empty")
     private String password;
-
-    @NotBlank(message = "First name cannot be empty")
-    private String firstName;
-
-    @NotBlank(message = "Last name cannot be empty")
-    private String lastName;
 }
